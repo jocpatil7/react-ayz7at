d76fb64d292e46fa4ea05ad1e5a0a002270812e4
jocpatil7 <jocpatil7@gmail.com>
2020-10-15 12:43:51 +0530

    2 files modified

--- src/Counter/counter.js
+++ src/Counter/counter.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
 
-class Counter extends React.Component{
-  
-  render(){
-    return(<React.Frafment>
-    <p>Counter Value is 0</p>
-    </React.Frafment>)
+class Counter extends React.Component {
+
+constructor(props){
+  super(props)
+this.state={
+  counterValue : 0
+}
+}
+  onIncrementClick =(value=0)=>{
+    //console.log(this.state.counterValue)
+    value=value+1;
+    this.setState({counterValue : value})
+    console.log(value);
+    //alert("I am Increment Click " + value);
+    }
+  render() {
+    return (
+      <div>
+      {this.state.counterValue>0 && 
+        <p>Counter Value is {this.state.counterValue}</p>}
+        <input type="button" value="Increment" onClick={ (value) => this.onIncrementClick(this.state.counterValue)}/>
+
+        </div>
+     
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import Counter from "./Counter/counter";
 
 
 ReactDOM.render(<Counter />, document.getElementById("root"));
